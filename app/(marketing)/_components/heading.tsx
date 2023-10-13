43ecfc7abef52587473e0b5c23c03a27c2490092
Samarth Sheth <samarth.sheth.29@gmail.com>
2023-10-13 18:30:30 +0530

    authentication

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -2,10 +2,15 @@
 
 import Link from "next/link"
 import { ArrowRight } from "lucide-react"
+import { useConvexAuth } from "convex/react"
+import { SignUpButton } from "@clerk/clerk-react"
 
+import { Spinner } from "@/components/spinner"
 import { Button } from "@/components/ui/button"
 
 export const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth()
+
   return (
     <header className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -16,12 +21,27 @@ export const Heading = () => {
         Notion is the connected workspace where <br />
         better, faster work happens.
       </h3>
-      <Button asChild>
-        <Link href="/documents">
-          Enter Notion
-          <ArrowRight className="h-4 w-4 ml-2" />
-        </Link>
-      </Button>
+      {isLoading && (
+        <div className="w-full flex items-center justify-center">
+          <Spinner size="lg" />
+        </div>
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href="/documents">
+            Enter Notion
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Link>
+        </Button>
+      )}
+      {!isAuthenticated && !isLoading && (
+        <SignUpButton mode="modal">
+          <Button>
+            Get Notion free
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Button>
+        </SignUpButton>
+      )}
     </header>
   )
 }
--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,13 +1,20 @@
 "use client"
 
-import { cn } from "@/lib/utils"
-import { useScrollTop } from "@/hooks/use-scroll-top"
+import Link from "next/link"
+import { ArrowRight } from "lucide-react"
+import { useConvexAuth } from "convex/react"
+import { SignInButton, SignUpButton, UserButton } from "@clerk/clerk-react"
 
+import { cn } from "@/lib/utils"
+import { Spinner } from "@/components/spinner"
+import { Button } from "@/components/ui/button"
 import { ModeToggle } from "@/components/mode-toogle"
+import { useScrollTop } from "@/hooks/use-scroll-top"
 
 import { Logo } from "./logo"
 
 export const Navbar = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth()
   const scrolled = useScrollTop()
 
   return (
@@ -19,6 +26,29 @@ export const Navbar = () => {
     >
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Log in
+              </Button>
+            </SignInButton>
+            <SignUpButton mode="modal">
+              <Button size="sm">Get Notion free</Button>
+            </SignUpButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm" asChild>
+              <Link href="/documents">
+                Enter Notion <ArrowRight className="h-4 w-4 ml-2" />
+              </Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
